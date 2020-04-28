45669aeea99e1bac60498e2820fc77091f16afb2
Favourcodes <falbassey4life@gmail.com>
2020-04-28 14:28:37 +0100

    I restructured the shopping app by adding some features to the products

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from. models import Product
 
-
-# Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 # To add a new page to the django site
